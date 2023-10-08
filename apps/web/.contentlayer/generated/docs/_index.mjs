bd282adea20fcefcede4d785d023f30ad90b1481
Skyler.Ab <85718273+SklyerX@users.noreply.github.com>
2023-10-08 16:24:05 -0400

    folder docs -> Docs

--- apps/web/.contentlayer/generated/docs/_index.mjs
+++ apps/web/.contentlayer/generated/docs/_index.mjs
@@ -2,7 +2,7 @@
 
 import docs__gettingStarted__introductionMdx from './docs__getting-started__introduction.mdx.json' assert { type: 'json' }
 import docs__gettingStarted__setupMdx from './docs__getting-started__setup.mdx.json' assert { type: 'json' }
-import docs__technology__faqMdx from './docs__technology__faq.mdx.json' assert { type: 'json' }
 import docs__prerequisites__environmentSetupMdx from './docs__prerequisites__environment-setup.mdx.json' assert { type: 'json' }
+import docs__technology__faqMdx from './docs__technology__faq.mdx.json' assert { type: 'json' }
 
-export const allDocs = [docs__gettingStarted__introductionMdx, docs__gettingStarted__setupMdx, docs__technology__faqMdx, docs__prerequisites__environmentSetupMdx]
+export const allDocs = [docs__gettingStarted__introductionMdx, docs__gettingStarted__setupMdx, docs__prerequisites__environmentSetupMdx, docs__technology__faqMdx]
